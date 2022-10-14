2d32cb6fd84ca9f8ea2f1eeb52c2507b73a487c5
Semen Trofimov <semiony@gmail.com>
2022-10-14 14:26:37 +0300

    added

--- project/main.go
+++ project/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"log"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
+	var output1 string
 	myApp := app.New()
-	w := myApp.NewWindow("Image")
+	myWindow := myApp.NewWindow("Grid Layout")
 
-	// image := canvas.NewImageFromResource(theme.FyneLogo())
-	// image := canvas.NewImageFromURI(uri)
-	// image := canvas.NewImageFromImage(src)
-	// image := canvas.NewImageFromReader(reader, name)
-	image := canvas.NewImageFromFile("./image.png")
-	image.FillMode = canvas.ImageFillOriginal
-	w.SetContent(image)
+	button1 := widget.NewButton("button1", func() {
+		log.Println("tapped button1")
+		output1 = "button1"
+	})
+	button2 := widget.NewButton("button2", func() {
+		log.Println("tapped button2")
+	})
+	button3 := widget.NewButton("button3", func() {
+		log.Println("tapped button3")
+	})
+	button4 := widget.NewButton("button4", func() {
+		log.Println("tapped button4")
+	})
 
-	w.ShowAndRun()
+	label1 := widget.NewLabel(output1)
+
+	grid := container.New(layout.NewGridLayout(2), button1, button2, button3, button4, label1)
+	myWindow.SetContent(grid)
+	myWindow.Resize(fyne.NewSize(350, 350))
+	myWindow.ShowAndRun()
 }
