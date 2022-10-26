7165623619f8f444ff0384cc5a8d1bb8af6f32a3
strofimo <sermiony@gmail.com>
2022-10-26 16:26:24 +0300

    added consumer

--- project/main.go
+++ project/main.go
@@ -32,36 +32,45 @@ func main() {
 	button1 := widget.NewButton("Send Message for RabbitMq", func() {
 		log.Println("content was", input1.Text, input2.Text, input3.Text)
 
-		conn, err := amqp.Dial(input1.Text)
-		fail.OnError(err, "Failed to connect to RabbitMQ")
-		defer conn.Close()
+		connect, err := amqp.Dial(input1.Text)
+		fail.OnError(err, "Failed to connect rabbit")
+		defer connect.Close()
 
-		ch, err := conn.Channel()
-		fail.OnError(err, "Failed to open a channel")
+		ch, err := connect.Channel()
+		fail.OnError(err, "Failed to connect a chanel")
 		defer ch.Close()
 
-		q, err := ch.QueueDeclare(
-			input2.Text, // name
-			false,       // durable
-			false,       // delete when unused
-			false,       // exclusive
-			false,       // no-wait
-			nil,         // arguments
+		qa, err := ch.QueueDeclare(
+			input2.Text,
+			false,
+			false,
+			false,
+			false,
+			nil,
 		)
-		fail.OnError(err, "Failed to declare a queue")
+		fail.OnError(err, "Failed declara a queue")
 
-		// body := "Hello World!"
-		err = ch.Publish(
-			"",     // exchange
-			q.Name, // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(input3.Text),
-			})
-		log.Printf(" [x] Sent %s", input3.Text)
-		fail.OnError(err, "Failed to publish a message")
+		msgs, err := ch.Consume(
+			qa.Name,
+			"",
+			true,
+			false,
+			false,
+			false,
+			nil,
+		)
+		fail.OnError(err, "Failed to register a consumer")
+
+		forever := make(chan bool)
+
+		go func() {
+			for d := range msgs {
+				log.Printf("Received a message: %s", d.Body)
+			}
+		}()
+
+		log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+		<-forever
 	})
 	grid := container.New(layout.NewGridLayout(1), input1, input2, input3, button1)
 	myWindow.SetContent(grid)
