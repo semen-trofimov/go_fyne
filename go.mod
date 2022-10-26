e00f9790445d4e802797e5aa0e117279bbd0282d
strofimo <sermiony@gmail.com>
2022-10-26 16:20:42 +0300

    test complite

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module fyne_app
 
 go 1.18
 
-require fyne.io/fyne/v2 v2.2.3
+require (
+	fyne.io/fyne/v2 v2.2.3
+	github.com/streadway/amqp v1.0.0 // indirect
+)
--- project/main.go
+++ project/main.go
@@ -8,9 +8,14 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+
+	"fyne_app/internal/fail"
+
+	"github.com/streadway/amqp"
 )
 
 func main() {
+
 	myApp := app.New()
 
 	input1 := widget.NewEntry()
@@ -24,16 +29,42 @@ func main() {
 
 	myWindow := myApp.NewWindow("Grid Layout")
 
-	button1 := widget.NewButton("save", func() {
+	button1 := widget.NewButton("Send Message for RabbitMq", func() {
 		log.Println("content was", input1.Text, input2.Text, input3.Text)
+
+		conn, err := amqp.Dial(input1.Text)
+		fail.OnError(err, "Failed to connect to RabbitMQ")
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+		fail.OnError(err, "Failed to open a channel")
+		defer ch.Close()
+
+		q, err := ch.QueueDeclare(
+			input2.Text, // name
+			false,       // durable
+			false,       // delete when unused
+			false,       // exclusive
+			false,       // no-wait
+			nil,         // arguments
+		)
+		fail.OnError(err, "Failed to declare a queue")
+
+		// body := "Hello World!"
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(input3.Text),
+			})
+		log.Printf(" [x] Sent %s", input3.Text)
+		fail.OnError(err, "Failed to publish a message")
 	})
-
-	// button1 := widget.NewButton("button1", func() {
-	// 	log.Println("tapped button1")
-	// })
-
 	grid := container.New(layout.NewGridLayout(1), input1, input2, input3, button1)
 	myWindow.SetContent(grid)
-	myWindow.Resize(fyne.NewSize(350, 350))
+	myWindow.Resize(fyne.NewSize(800, 350))
 	myWindow.ShowAndRun()
 }
